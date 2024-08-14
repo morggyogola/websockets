fee477af8ac0ecd42edcb6abcb6ff7da554c3058
Morgan Ogola <morganogola@gmail.com>
2024-08-14 16:27:40 +0300

    added websocket and rector-netty dependency

---
+++ src/main/java/com/example/websockets2/models/Hello.java
@@ -0,0 +1,13 @@
+/*
+package com.example.websockets2.models;
+
+public class Hello {
+
+    private String name;
+
+    public enum Status{
+        JOINED("joined"),
+        LEFT("left");
+    }
+}
+*/
