d8f55f913b7f3e413a7e8f6c0dcf320238c182ff
Morgan Ogola <morganogola@gmail.com>
2024-08-14 15:37:50 +0300

    First commit

---
+++ src/main/java/com/example/websockets2/Websockets2Application.java
@@ -0,0 +1,13 @@
+package com.example.websockets2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websockets2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websockets2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websockets2/Websockets2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websockets2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Websockets2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
